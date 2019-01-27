0b7735cd54b96a409873e1123eff5cc53fa5e3d5
wontons <antonsawka@hotmail.com>
2019-01-27 16:18:27 -0600

    made this shit work properly (still not finished though)

--- code.js
+++ code.js
@@ -10,31 +10,47 @@
 /**
  * Constants
  */
-const CIRCLE_WIDTH = 75;
-const DIST = 500;
+const PADDING = 25;
 
 /**
  * Public Variables
  */
-var ctx;
+var times = "";
 var dists = [100,200,400];
 var widths = [50,100,200];
+var ctx, randomXEnd, randomYEnd, randomXStart, randomYStart, canvas, width, height, bigGreen, bigGreen2, trialNumber, _width, _dist;
+trialNumber = _width = _dist = 0;
 
+$(document).ready(function() {
+  var _done, _start, _end;
+  _done = _start = _end = 0;
+  init();
+  generatePoints(0,0);
 
-/**
- * Circle Spawning
- */
-BigCircleS = function(ctx,x, y, color, circleSize) {
-    ctx.beginPath();
-    ctx.arc(x, y, circleSize, 0, Math.PI * 2, true);
-    ctx.fillStyle=color
-    ctx.fill();
-    ctx.closePath();
-    this.clicked=function(){
-        console.log("Start Clicked")
-        timer();
+  $('#canvas').click(function(e){
+    var x = e.clientX, y = e.clientY;
+
+    if(Math.pow(x-randomXStart,2)+Math.pow(y-randomYStart,2) < Math.pow(widths[0],2)){
+      bigGreen.clicked();
+      _start = 1;
+    } 
+
+    if(Math.pow(x-randomXEnd,2)+Math.pow(y-randomYEnd,2) < Math.pow(widths[0],2)){
+      if(_start == 1){
+        times += trialNumber + ". " + bigGreen2.clicked() + "\n";
+        _end = 1;
+      }
     }
-};
+
+    if(_start == 1 && _end == 1){
+      _start = _end = 0;
+      console.log("resetting canvas... " + trialNumber);
+      trialNumber+=10;
+      nextTrial();
+    }
+  })
+});
+
 BigCircleStart = function(ctx,x, y, color, circleSize) {
     ctx.beginPath();
     ctx.arc(x, y, circleSize, 0, Math.PI * 2, true);
@@ -55,80 +71,97 @@ BigCircleEnd = function(ctx,x, y, color, circleSize) {
     ctx.closePath();
     this.clicked=function(){
         console.log("End Clicked")
-        stopTimer();
+        return stopTimer();
     }
 };
 
-
-
-
-/**
- * Document initilization
- */
-function init() {
-  var distCheck = 0;
-  var randomXEnd = 0;
-  var randomYEnd = 0;
-  var randomXStart = 0;
-  var randomYStart = 0;
-  var canvas = document.getElementsByTagName("canvas")[0];
-  // Set canvas to device size
-  //var timerHeight = document.getElementById('timer').offsetHeight;
-  //console.log(offsetHeight);
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;// - timerHeight;
-  var width = canvas.width;
-  var height = canvas.height;
-
-  var randStart = getRandomInRange(0,4);
-  if(randStart < 1){
-    randomXStart = widths[0];
-    randomYStart = widths[0];
-    var rand = getRandomInRange(1,dists[0] - 1 + widths[0]);
-    randomXEnd = randomXStart + rand;
-    randomYEnd = randomYStart + Math.sqrt( Math.pow(dists[0] + widths[0],2) - Math.pow(rand,2) );
-  } else if(randStart < 2){
-    randomXStart = widths[0];
-    randomYStart = height - widths[0];
-    var rand = getRandomInRange(1,dists[0] - 1 + widths[0]);
-    randomXEnd = randomXStart + rand;
-    randomYEnd = randomYStart - Math.sqrt( Math.pow(dists[0] + widths[0],2) - Math.pow(rand,2) );
-  } else if(randStart < 3){
-    randomXStart = width - widths[0];
-    randomYStart = widths[0];
-    var rand = getRandomInRange(1,dists[0] - 1 + widths[0]);
-    randomXEnd = randomXStart - rand;
-    randomYEnd = randomYStart + Math.sqrt( Math.pow(dists[0] + widths[0],2) - Math.pow(rand,2) );
-  } else if(randStart < 4){
-    randomXStart = width - widths[0];
-    randomYStart = height - widths[0];
-    var rand = getRandomInRange(1,dists[0] - 1 + widths[0]);
-    randomXEnd = randomXStart - rand;
-    randomYEnd = randomYStart - Math.sqrt( Math.pow(dists[0] + widths[0],2) - Math.pow(rand,2) );
-  }
-
+function init(){
+  canvas = document.getElementsByTagName("canvas")[0];
+  width = canvas.width = window.innerWidth;
+  height = canvas.height = window.innerHeight;
   ctx = canvas.getContext('2d');
-  var bigGreen = new BigCircleStart(ctx, randomXStart, randomYStart, '#5eb62b', widths[0]);
-  var bigGreen2 = new BigCircleEnd(ctx, randomXEnd, randomYEnd, '#ff0000', widths[0]);
-  $('#canvas').click(function(e){
-    var x = e.clientX;
-    var y = e.clientY;
-    if(Math.pow(x-randomXStart,2)+Math.pow(y-randomYStart,2) < Math.pow(widths[0],2))   
-      bigGreen.clicked();
-
-    if(Math.pow(x-randomXEnd,2)+Math.pow(y-randomYEnd,2) < Math.pow(widths[0],2))   
-      bigGreen2.clicked();
-  })
-
 }
 
-/**
- * Returns a random number between min (inclusive) and max (exclusive)
- */
 function getRandomInRange(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-$(document).ready(function() {
-    init();   
-});
+function resetCanvas(){
+  ctx.clearRect(0, 0, width, height);
+}
+
+function generatePoints(_width, _dist){
+  var randStart = getRandomInRange(0,4);
+  var rand;
+
+  if(randStart < 1){
+    randomXStart = widths[_width] + PADDING;
+    randomYStart = widths[_width] + PADDING;
+    rand = getRandomInRange(1,dists[_dist] - 1 + widths[_width]);
+    randomXEnd = randomXStart + rand + PADDING;
+    randomYEnd = randomYStart + Math.sqrt( Math.pow(dists[_dist] + widths[_width],2) - Math.pow(rand,2) ) + PADDING;
+  } else if(randStart < 2){
+    randomXStart = widths[_width] + PADDING;
+    randomYStart = height - widths[_width] - PADDING;
+    rand = getRandomInRange(1,dists[_dist] - 1 + widths[_width]);
+    randomXEnd = randomXStart + rand + PADDING;
+    randomYEnd = randomYStart - Math.sqrt( Math.pow(dists[_dist] + widths[_width],2) - Math.pow(rand,2) ) - PADDING;
+  } else if(randStart < 3){
+    randomXStart = width - widths[_width] - PADDING;
+    randomYStart = widths[_width] + PADDING;
+    rand = getRandomInRange(1,dists[_dist] - 1 + widths[_width]);
+    randomXEnd = randomXStart - rand - PADDING;
+    randomYEnd = randomYStart + Math.sqrt( Math.pow(dists[_dist] + widths[_width],2) - Math.pow(rand,2) ) + PADDING;
+  } else if(randStart < 4){
+    randomXStart = width - widths[_width] - PADDING;
+    randomYStart = height - widths[_width] - PADDING;
+    rand = getRandomInRange(1,dists[_dist] - 1 + widths[_width]);
+    randomXEnd = randomXStart - rand - PADDING;
+    randomYEnd = randomYStart - Math.sqrt( Math.pow(dists[_dist] + widths[_width],2) - Math.pow(rand,2) ) - PADDING;
+  }
+
+  // widths[_width]/2 : function wants radius NOT diameter
+  bigGreen = new BigCircleStart(ctx, randomXStart, randomYStart, '#5eb62b', widths[_width]/2);
+  bigGreen2 = new BigCircleEnd(ctx, randomXEnd, randomYEnd, '#ff0000', widths[_width]/2);
+}
+
+function nextTrial(){
+  resetCanvas();
+  switch(trialNumber){
+    case 10:
+      _dist = 1;  break;
+    case 20:
+      _dist = 2;  break;
+    case 30:
+      _dist = 0;  _width = 1; break;
+    case 40:
+      _dist = 1;  break;
+    case 50:
+      _dist = 2;  break;
+    case 60:
+      _dist = 0;  _width = 2; break;
+    case 70:
+      _dist = 1;  break;
+    case 80:
+      _dist = 2;  break;
+    case 90:
+      drawEnd();  console.log("Results:\n\n1. " + trialNumber); alert(times);  break;
+  }
+
+  if(trialNumber != 90){
+    generatePoints(_width,_dist);
+  }
+}
+
+function drawEnd(){
+  resetCanvas();
+  ctx.beginPath();
+  ctx.arc(width/2, height/2, 250, 0, Math.PI * 2, true);
+  ctx.fillStyle="#ff00ff";
+  ctx.fill();
+  ctx.closePath();
+  ctx.font = "40px Ariel";
+  ctx.fillStyle = "black";
+  ctx.textAlign = "center";
+  ctx.fillText("Thank you for participating!",width/2,height/2);
+}
--- stopwatch.js
+++ stopwatch.js
@@ -6,7 +6,7 @@
  */
 
  
-var h1 = document.getElementsByTagName('h1')[0];
+//var h1 = document.getElementsByTagName('h1')[0];
 var seconds = 0;
 var minutes = 0;
 var hours = 0;
@@ -22,7 +22,7 @@ function add() {
             hours++;
         }
     }
-    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+    //h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
 
     timer();
 }
@@ -31,17 +31,19 @@ function timer() {
 }
 
 function stopTimer() {
-    alert(h1.textContent);
+    //alert(h1.textContent);
+    var _time = seconds;
     clearTimer();
+    return _time;
 }
 
 function clearTimer() {
 	clearTimeout(t);
-	h1.textContent = "00:00:00";
+	//h1.textContent = "00:00:00";
     seconds = 0; minutes = 0; hours = 0;
 }
 
 $(document).ready(function() {
-	h1 = document.getElementsByTagName('h1')[0];
+	//h1 = document.getElementsByTagName('h1')[0];
     //timer();
 });
